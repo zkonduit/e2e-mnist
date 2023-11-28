539b3947ff8da4e286f521aa1337e337462791a8
Casey <leptone55@gmail.com>
2023-11-27 18:33:05 -0800

    fix: working

--- components/mnist-draw/MNISTDraw.tsx
+++ components/mnist-draw/MNISTDraw.tsx
@@ -220,16 +220,19 @@ export function MNISTDraw() {
 
       let getProofResp
       while (status !== 'SUCCESS') {
-        await new Promise((resolve) => setTimeout(resolve, 5_000))
-
         getProofResp = await hub.getProof({
           id,
           url,
         })
 
         status = getProofResp.status
-        console.log('getProofResp', getProofResp)
+
+        if (status === 'SUCCESS') {
+          break
+        }
+        await new Promise((resolve) => setTimeout(resolve, 2_000))
       }
+      console.log('getProofResp', getProofResp)
 
       const p = BigInt(
         '0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001'
